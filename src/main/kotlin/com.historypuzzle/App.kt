839f20d041fcc7d23ba597e2578d616de3d02bf1
El gran <ferran@elgran.cat>
2020-03-19 22:58:50 +0100

    First commit

---
+++ src/main/kotlin/com.historypuzzle/App.kt
@@ -0,0 +1,46 @@
+package com.historypuzzle
+
+import ratpack.handling.Context
+import ratpack.server.BaseDir
+
+
+fun main() {
+    createServer().start()
+}
+
+private fun createServer() = serverOf {
+    serverConfig {
+        baseDir(BaseDir.find())
+    }
+
+    handlers {
+        path("foo") { render("from the foo handler") }
+        path("bar") { render("from the bar handler") }
+
+        // Map to /baz using a Kotlin function
+        path("baz", ::bazHandler)
+
+        // Set up a nested routing block, which is delegated to `nestedHandler`
+        prefix("nested") {
+            path(":var1/:var2?") {
+                // The path tokens are the :var1 and :var2 path components above
+                val var1 = pathTokens["var1"]
+                val var2 = pathTokens["var2"]
+                render("from the nested handler, var1: $var1, var2: $var2")
+            }
+        }
+
+        // Map to a dependency injected handler
+        //path("injected", MyHandler::class.java)
+
+        // Bind the /static app path to the src/ratpack/assets/images dir
+        prefix("static") {
+            fileSystem("assets/images") { files() }
+        }
+
+        all { render("root handler!") }
+    }
+}
+
+/** A handler as a method */
+fun bazHandler(context: Context) = context.render("from the baz handler")
