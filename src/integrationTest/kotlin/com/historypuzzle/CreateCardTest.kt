a5b7f18d398e5bf69c0d9cd734da7dbe8b1f2add
El gran <ferran@elgran.cat>
2020-04-04 18:49:37 +0200

    Integration tests
    - Integration tests source set added to the project
    - Capacity of addind properties from test to connecto to a different db

--- src/integrationTest/kotlin/com/historypuzzle/CreateCardTest.kt
+++ src/integrationTest/kotlin/com/historypuzzle/CreateCardTest.kt
@@ -1,16 +1,39 @@
 package com.historypuzzle
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.amshove.kluent.shouldBeEqualTo
-import org.amshove.kluent.shouldContain
+import org.jdbi.v3.core.Jdbi
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.gherkin.Feature
+import org.spekframework.spek2.style.specification.describe
 import ratpack.test.embed.EmbeddedApp
 
 object CreateCardTest : Spek({
 
+    describe("tests test") {
+        it("patata") {
+            3 shouldBeEqualTo 3
+        }
+    }
     lateinit var app: EmbeddedApp
 
     beforeGroup {
+        /**
+         * This should set up a specific db for integration test
+         */
+        val db = "history_puzzle"
+        val user = "local-user"
+        val password = "local-password"
+        val url = "jdbc:postgresql://localhost:5432/$db"
+        System.setProperty("ratpack.database.db", db)
+        System.setProperty("ratpack.database.user", "local-user")
+        System.setProperty("ratpack.database.password", "local-password")
+
+        val jdbi = Jdbi.create(url, user, password)
+
+        jdbi.useHandle<Exception> {
+            it.createUpdate("DELETE FROM card").execute()
+        }
         app = EmbeddedApp.fromServer(createServer())
     }
 
@@ -20,7 +43,6 @@ object CreateCardTest : Spek({
 
     Feature("Post new card") {
         Scenario("Happy path") {
-            val jsonResult = """{"id":4,"title":"World War I","year":1914,"month":7,"day":28,"picture":"","wikipedia":"https://en.wikipedia.org/wiki/World_War_I","info":""}"""
             When("Correct json is send") {
                 app.test { httpClient ->
                     val json = """
@@ -36,7 +58,15 @@ object CreateCardTest : Spek({
                 """
                     httpClient.postJson("card", json) {
                         statusCode shouldBeEqualTo 200
-                        body.text shouldBeEqualTo jsonResult
+                        val jsonMap = ObjectMapper().readValue(body.text, Map::class.java)
+                        jsonMap.keys.size shouldBeEqualTo 8
+                        jsonMap["title"] shouldBeEqualTo "World War I"
+                        jsonMap["year"] shouldBeEqualTo 1914
+                        jsonMap["month"] shouldBeEqualTo  7
+                        jsonMap["day"] shouldBeEqualTo  28
+                        jsonMap["imageLink"] shouldBeEqualTo  ""
+                        jsonMap["info"] shouldBeEqualTo  ""
+                        jsonMap["wikipediaLink"] shouldBeEqualTo  "https://en.wikipedia.org/wiki/World_War_I"
                     }
                 }
             }
@@ -44,7 +74,17 @@ object CreateCardTest : Spek({
                 app.test { httpClient ->
                     httpClient.get("card") {
                         statusCode shouldBeEqualTo 200
-                        body.text shouldContain  jsonResult
+                        val jsonList = ObjectMapper().readValue(body.text, List::class.java)
+                        jsonList.size shouldBeEqualTo 1
+                        val jsonMap = jsonList[0] as Map<String, Any>
+                        jsonMap.keys.size shouldBeEqualTo 8
+                        jsonMap["title"] shouldBeEqualTo "World War I"
+                        jsonMap["year"] shouldBeEqualTo 1914
+                        jsonMap["month"] shouldBeEqualTo  7
+                        jsonMap["day"] shouldBeEqualTo  28
+                        jsonMap["imageLink"] shouldBeEqualTo  ""
+                        jsonMap["info"] shouldBeEqualTo  ""
+                        jsonMap["wikipediaLink"] shouldBeEqualTo  "https://en.wikipedia.org/wiki/World_War_I"
                     }
                 }
             }
--- src/main/kotlin/com/historypuzzle/infrastructure/module/DatabaseModule.kt
+++ src/main/kotlin/com/historypuzzle/infrastructure/module/DatabaseModule.kt
@@ -3,6 +3,7 @@ package com.historypuzzle.infrastructure.module
 import com.google.inject.AbstractModule
 import com.google.inject.Provides
 import com.historypuzzle.DatabaseConfig
+import com.historypuzzle.common.getLogger
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import org.jdbi.v3.core.Jdbi
@@ -10,12 +11,17 @@ import org.jdbi.v3.core.kotlin.KotlinPlugin
 import javax.sql.DataSource
 
 class DatabaseModule : AbstractModule() {
+    private companion object {
+        val log = getLogger<DatabaseModule>()
+    }
     override fun configure() {
 
     }
 
     @Provides
     fun dataSource(databaseConfig: DatabaseConfig): HikariDataSource {
+        log.info("Creating datasource with config:")
+        log.info(databaseConfig.toString())
         val config = HikariConfig()
         config.jdbcUrl = "jdbc:postgresql://${databaseConfig.host}/${databaseConfig.db}"
         config.username = databaseConfig.user
